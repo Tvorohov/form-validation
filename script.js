f0905255e37ba88f19bcbb6f51e4f1699086a2d1
Александр Творогов <tvorogov100500@gmail.com>
2020-01-09 22:32:54 +0200

    add form validation,  add readme

---
+++ script.js
@@ -0,0 +1,71 @@
+'use strict';
+
+function validateForm(options) {
+  const {
+    formId,
+    formValidClass,
+    formInvalidClass,
+    inputErrorClass
+  } = options;
+  const form = document.getElementById(formId);
+  const inputs = [...form.querySelectorAll('input')];
+
+  inputs.forEach((el => {
+    el.addEventListener('blur', validateInput, true);
+    el.addEventListener('focus', removeErrorClass, true)
+  }));
+  form.addEventListener('submit', submitFields);
+
+  function removeErrorClass() {
+    if (this.classList.contains(inputErrorClass)) {
+      this.classList.remove(inputErrorClass);
+    }
+  }
+
+  function validateInput() {
+    const {
+      validator,
+      validatorMin,
+      validatorMax,
+      validatorPattern
+    } = this.dataset;
+    const value = this.value;
+    switch (validator) {
+      case 'letters':
+        let lettersMatch = !value.match(/^[a-zа-яё]+$/i);
+        this.classList.toggle(inputErrorClass, lettersMatch);
+        break;
+      case 'number':
+        if (validatorMin &&
+          validatorMax) {
+          //в качестве второго аргумента в метод toggle передаем условия
+          //первым условием проверяем неравенство длины строки нулю, так как Number('') == 0
+          let numberMinMaxMatch = value.length === 0 || isNaN(value) ||
+            Number(value) < validatorMin || Number(value) > validatorMax;
+          this.classList.toggle(inputErrorClass, numberMinMaxMatch);
+        } else {
+          let numberMatch = !this.value.match(/^-?\d+$/i)
+          this.classList.toggle(inputErrorClass, numberMatch);
+        }
+        break;
+      case 'regexp':
+        let regexpMatch = !value.match(validatorPattern)
+        this.classList.toggle(inputErrorClass, regexpMatch);
+        break;
+    }
+  }
+
+  function submitFields(e) {
+    e.preventDefault();
+    //проверяем только те элементы, которые имеют мета-атрибут 'data-required'
+    inputs.forEach(el => {
+      if (el.value.length === 0 && el.dataset.hasOwnProperty('required')) validateInput.call(el);
+    });
+    const isHasError = elem => elem.classList.contains(inputErrorClass);
+    const isValid = !inputs.some(isHasError);
+    console.log(isValid)
+    this.classList.toggle(formInvalidClass, !isValid);
+    this.classList.toggle(formValidClass, isValid);
+    // if (isValid) this.submit();
+  }
+};
